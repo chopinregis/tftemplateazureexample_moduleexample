f75ede0cd4e8ded97bda532684729c2112e78dd0
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-31 15:46:43 -0400

    Create main.tf

---
+++ examplemodule/main.tf
@@ -0,0 +1,21 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnets
+resource "azurerm_subnet" "subnet_test" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.address_prefixes
+}
